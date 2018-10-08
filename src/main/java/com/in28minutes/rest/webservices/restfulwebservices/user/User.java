30fd966acbc6e001ef61ef8a6c9a8923885bb214
GUANH <heran.guan@toyota.com>
2018-10-08 18:37:12 +0100

    update

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -1,11 +1,19 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue
     private Integer id;
+
     @Size(min=2,message="Name should have at least 2 characters")
     private String name;
     @Past
